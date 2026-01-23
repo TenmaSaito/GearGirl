73f7416d129929c4906b6205c0b63d5fe28480a5
Endo Hideto <251309@st.yoshida-g.ac.jp>
2026-01-23 17:20:01 +0900

    カメラリリースを追加

--- camera.h
+++ camera.h
@@ -86,6 +86,7 @@ void SetCamera(void);										// カメラを設置（mainのDrawの最初にする）
 void SetPotisionCamera(int nIdx, vec3 pos);					// カメラの位置更新
 void CameraEnable(int nIdx);								// カメラを描画に使用すると宣言
 int GetCamera(void);										// まだ未使用のカメラ番号を取得
+void ReleaseCamera(int nIdx);								// カメラを開放
 P_CAMERA GetCameraArray(void);								// カメラ除法配列の先頭アドレスを取得
 void GetCameraPos(int nCamNum, vec3* pPosV, vec3* pPosR);	// カメラの位置情報を取得
 void GetCameraRot(int nCamNum, vec3* pRot);					// カメラの角度情報を取得
