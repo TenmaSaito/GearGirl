7bd3d67d9045b872f640813e83c8a0d23e79868c
KikuchiMina <251313@st.yoshida-g.ac.jp>
2026-02-03 10:11:38 +0900

    ポーズの選択肢を変更したとき、勝手に整列できるようになりました

--- pause.cpp
+++ pause.cpp
@@ -11,17 +11,24 @@
 #include "pause.h"
 #include "time.h"
 
+//==========================================================================================
+// ****** ポーズの選択肢を変更するとき ******
+// "ヘッダー"、"テクスチャファイル名"、"UpdatePauseのENTERキーが押された場合"の3つを変更
+//==========================================================================================
+
 //=========================================================
 //マクロ定義
 //=========================================================
-#define MAX_POUSE	(PAUSE_MENU_MAX)		//ポーズの種類
+#define MAX_POUSE		PAUSE_MENU_MAX		//ポーズの種類
+#define HEIGHT_SIZE		100.0f 				//縦サイズ
+#define WIDTH_SIZE		600.0f				//横サイズ
+#define LINE_SPACE		50.0f				//行間隔
 
 //=========================================================
 //グローバル変数
 //=========================================================
 LPDIRECT3DTEXTURE9		g_apTexturePause[MAX_POUSE]	 = {};		//テクスチャへのポインタ
 LPDIRECT3DVERTEXBUFFER9	g_pVtxBuffPause				 = NULL;	//頂点バッファへのポインタ
-PAUSE_MENU				g_PauseMenu;							//ポーズメニュー
 D3DXVECTOR3 g_posPause[MAX_POUSE];								//位置
 bool nUpDisp = false;
 int nUpCounter = 0;
@@ -44,6 +51,7 @@ void InitPause(void)
 {
 	LPDIRECT3DDEVICE9 pDevice = GetDevice();		//デバイスへのポインタの取得
 	VERTEX_2D* pVtx;								//頂点情報へのポインタ
+	float fDrawStart = 0.0f;
 
 	//-------------------------------------
 	//テクスチャの読み込み
@@ -53,9 +61,12 @@ void InitPause(void)
 	}
 
 	//-------------------------------------
+	//計算
+	fDrawStart = SCREEN_HEIGHT / 2 - (PAUSE_MENU_MAX * HEIGHT_SIZE + (PAUSE_MENU_MAX - 1) * LINE_SPACE) / 2;
+
+	//-------------------------------------
 	//初期化
 	nPauseData = 0;
-	g_PauseMenu = PAUSE_MENU_RESTART;
 	for (int nCntPouse = 0; nCntPouse < MAX_POUSE; nCntPouse++)
 	{
 		g_posPause[nCntPouse] = D3DXVECTOR3(640.0f, 0.0f, 0.0f);
@@ -64,11 +75,11 @@ void InitPause(void)
 	//-------------------------------------
 	//頂点バッファの作成
 	pDevice->CreateVertexBuffer(sizeof(VERTEX_2D) * 4 * MAX_POUSE,
-								D3DUSAGE_WRITEONLY,
-								FVF_VERTEX_2D,
-								D3DPOOL_MANAGED,
-								&g_pVtxBuffPause,
-								NULL);
+		D3DUSAGE_WRITEONLY,
+		FVF_VERTEX_2D,
+		D3DPOOL_MANAGED,
+		&g_pVtxBuffPause,
+		NULL);
 
 	//-------------------------------------
 	//頂点バッファをロックし、頂点情報へのポインタを獲得
@@ -77,11 +88,11 @@ void InitPause(void)
 	for (int nCntPause = 0; nCntPause < MAX_POUSE; nCntPause++)
 	{
 		//位置の設定
-		pVtx[0].pos = D3DXVECTOR3(g_posPause[nCntPause].x - 300.0f, nCntPause * 120.0f + 175.0f, 0.0f);
-		pVtx[1].pos = D3DXVECTOR3(g_posPause[nCntPause].x + 300.0f, nCntPause * 120.0f + 175.0f, 0.0f);
-		pVtx[2].pos = D3DXVECTOR3(g_posPause[nCntPause].x - 300.0f, nCntPause * 120.0f + 275.0f, 0.0f);
-		pVtx[3].pos = D3DXVECTOR3(g_posPause[nCntPause].x + 300.0f, nCntPause * 120.0f + 275.0f, 0.0f);
-
+		pVtx[0].pos = D3DXVECTOR3(g_posPause[nCntPause].x - WIDTH_SIZE * 0.5f, nCntPause * (LINE_SPACE + HEIGHT_SIZE) + fDrawStart, 0.0f);
+		pVtx[1].pos = D3DXVECTOR3(g_posPause[nCntPause].x + WIDTH_SIZE * 0.5f, nCntPause * (LINE_SPACE + HEIGHT_SIZE) + fDrawStart, 0.0f);
+		pVtx[2].pos = D3DXVECTOR3(g_posPause[nCntPause].x - WIDTH_SIZE * 0.5f, nCntPause * (LINE_SPACE + HEIGHT_SIZE) + fDrawStart + HEIGHT_SIZE, 0.0f);		//間隔, 描画始め, サイズ
+		pVtx[3].pos = D3DXVECTOR3(g_posPause[nCntPause].x + WIDTH_SIZE * 0.5f, nCntPause * (LINE_SPACE + HEIGHT_SIZE) + fDrawStart + HEIGHT_SIZE, 0.0f);
+		
 		//rhwの設定
 		pVtx[0].rhw = 1.0f;
 		pVtx[1].rhw = 1.0f;
@@ -139,105 +150,75 @@ void UninitPause(void)
 //=========================================================
 void UpdatePause(void)
 {
-	//VERTEX_2D* pVtx;										//頂点情報へのポインタ
-	//FADE pFade = GetFade();									//フェード情報の取得
+	VERTEX_2D* pVtx;										//頂点情報へのポインタ
+	FADE pFade = GetFade();									//フェード情報の取得
 
-	////-------------------------------------
-	////頂点バッファをロックし、頂点情報へのポインタを獲得
-	//g_pVtxBuffPause->Lock(0, 0, (void**)&pVtx, 0);
+	//-------------------------------------
+	//頂点バッファをロックし、頂点情報へのポインタを獲得
+	g_pVtxBuffPause->Lock(0, 0, (void**)&pVtx, 0);
 
-	////頂点座標を進める
-	//pVtx += nPauseData * 4;
-	//pVtx[0].col = D3DCOLOR_RGBA(255, 255, 255, 155);
-	//pVtx[1].col = D3DCOLOR_RGBA(255, 255, 255, 155);
-	//pVtx[2].col = D3DCOLOR_RGBA(255, 255, 255, 155);
-	//pVtx[3].col = D3DCOLOR_RGBA(255, 255, 255, 155);
+	//頂点座標を進める
+	pVtx += nPauseData * 4;
+	pVtx[0].col = D3DCOLOR_RGBA(255, 255, 255, 155);
+	pVtx[1].col = D3DCOLOR_RGBA(255, 255, 255, 155);
+	pVtx[2].col = D3DCOLOR_RGBA(255, 255, 255, 155);
+	pVtx[3].col = D3DCOLOR_RGBA(255, 255, 255, 155);
 
-	////-------------------------------------
-	////頂点バッファをアンロック
-	//g_pVtxBuffPause->Unlock();
+	//-------------------------------------
+	//頂点バッファをアンロック
+	g_pVtxBuffPause->Unlock();
 
-	////頂点バッファをロックし、頂点情報へのポインタを獲得
-	//g_pVtxBuffPause->Lock(0, 0, (void**)&pVtx, 0);
+	//頂点バッファをロックし、頂点情報へのポインタを獲得
+	g_pVtxBuffPause->Lock(0, 0, (void**)&pVtx, 0);
 
-	////-------------------------------------
-	////W・Sキーが押された
-	//if (nPauseData >= -1 || nPauseData <= MAX_POUSE)
-	//{
-	//	if (GetJoypadRepeat(0, JOYKEY_UP) == true || GetJoypadRepeat(1, JOYKEY_UP) == true ||
-	//		GetKeyboardRepeat(DIK_W) == true || GetKeyboardRepeat(DIK_UP) == true)
-	//	{//Wキーが押された
-	//		switch (nPauseData)
-	//		{
-	//		case PAUSE_MENU_RESTART:
-	//			if (--nPauseData <= 0)
-	//			{
-	//				nPauseData = PAUSE_MENU_CLOSE;
-	//			}
-	//			break;
-	//		case PAUSE_MENU_RETITLE:
-	//			if (--nPauseData <= 0)
-	//			{
-	//				nPauseData = PAUSE_MENU_RESTART;
-	//			}
-	//			break;
-	//		case PAUSE_MENU_CLOSE:
-	//			if (--nPauseData == 1)
-	//			{
-	//				nPauseData = PAUSE_MENU_RETITLE;
-	//			}
-	//			break;
-	//		}
-	//	}
-	//	if (GetJoypadRepeat(0, JOYKEY_DOWN) == true || GetJoypadRepeat(1, JOYKEY_DOWN) == true ||
-	//		GetKeyboardRepeat(DIK_S) == true || GetKeyboardRepeat(DIK_DOWN) == true)
-	//	{//Sキーが押された
-	//		switch (nPauseData)
-	//		{
-	//		case PAUSE_MENU_RESTART:
-	//			if (++nPauseData == 1)
-	//			{
-	//				nPauseData = PAUSE_MENU_RETITLE;
-	//			}
-	//			break;
-	//		case PAUSE_MENU_RETITLE:
-	//			if (++nPauseData >= 2)
-	//			{
-	//				nPauseData = PAUSE_MENU_CLOSE;
-	//			}
-	//			break;
-	//		case PAUSE_MENU_CLOSE:
-	//			if (++nPauseData >= 2)
-	//			{
-	//				nPauseData = PAUSE_MENU_RESTART;
-	//			}
-	//			break;
-	//		}
-	//	}
+	//-------------------------------------
+	//下に向かうキーが押された
+	if (GetJoypadRepeat(0, JOYKEY_UP) == true || GetJoypadRepeat(1, JOYKEY_UP) == true
+		|| GetKeyboardRepeat(DIK_W) == true || GetKeyboardRepeat(DIK_UP) == true)
+	{
+		nPauseData--;
 
-	//	//頂点座標を進める
-	//	pVtx += nPauseData * 4;
-	//	pVtx[0].col = D3DCOLOR_RGBA(255, 255, 255, 255);
-	//	pVtx[1].col = D3DCOLOR_RGBA(255, 255, 255, 255);
-	//	pVtx[2].col = D3DCOLOR_RGBA(255, 255, 255, 255);
-	//	pVtx[3].col = D3DCOLOR_RGBA(255, 255, 255, 255);
-	//}
+		if (nPauseData < PAUSE_MENU_RESTART)	//0より小さくしない
+		{
+			nPauseData = PAUSE_MENU_MAX - 1;
+		}
+	}
 
-	////-------------------------------------
-	////Pキーが押された
-	//if (GetJoypadTrigger(0, JOYKEY_BACK) == true ||
-	//	GetJoypadTrigger(1, JOYKEY_BACK) == true ||
-	//	GetKeyboardTrigger(DIK_P) == true)
-	//{
-	//	for (int nCntPause = 0; nCntPause < MAX_POUSE; nCntPause++)
-	//	{
-	//		pVtx[0].col = D3DCOLOR_RGBA(255, 255, 255, 155);
-	//		pVtx[1].col = D3DCOLOR_RGBA(255, 255, 255, 155);
-	//		pVtx[2].col = D3DCOLOR_RGBA(255, 255, 255, 155);
-	//		pVtx[3].col = D3DCOLOR_RGBA(255, 255, 255, 155);
-	//	}
-	//	nPauseData = 0;
-	//}
+	//-------------------------------------
+	//上に向かうキーが押された
+	if (GetJoypadRepeat(0, JOYKEY_DOWN) == true || GetJoypadRepeat(1, JOYKEY_DOWN) == true
+		|| GetKeyboardRepeat(DIK_S) == true || GetKeyboardRepeat(DIK_DOWN) == true)
+	{
+		nPauseData++;
+
+		if (nPauseData >= PAUSE_MENU_MAX)	//総数より大きくしない
+		{
+			nPauseData = PAUSE_MENU_RESTART;
+		}
+	}
+
+	//頂点座標を進める
+	pVtx += nPauseData * 4;
+	pVtx[0].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+	pVtx[1].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+	pVtx[2].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+	pVtx[3].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+
+	//-------------------------------------
+	//Pキーが押された
+	if (GetJoypadTrigger(0, JOYKEY_BACK) == true
+		|| GetJoypadTrigger(1, JOYKEY_BACK) == true
+		|| GetKeyboardTrigger(DIK_P) == true)
+	{
+		for (int nCntPause = 0; nCntPause < MAX_POUSE; nCntPause++)
+		{
+			pVtx[0].col = D3DCOLOR_RGBA(255, 255, 255, 155);
+			pVtx[1].col = D3DCOLOR_RGBA(255, 255, 255, 155);
+			pVtx[2].col = D3DCOLOR_RGBA(255, 255, 255, 155);
+			pVtx[3].col = D3DCOLOR_RGBA(255, 255, 255, 155);
+		}
+		nPauseData = 0;
+	}
 
 	////-------------------------------------
 	////ENTERキーが押された
@@ -257,8 +238,8 @@ void UpdatePause(void)
 	//	}
 	//}
 
-	////頂点バッファをアンロック
-	//g_pVtxBuffPause->Unlock();
+	//頂点バッファをアンロック
+	g_pVtxBuffPause->Unlock();
 }
 
 //=========================================================
