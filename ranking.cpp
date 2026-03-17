599e5eb48fafc879fed3cbabc5f64b99e70517d9
Endo Hideto <251309@st.yoshida-g.ac.jp>
2026-03-17 17:52:40 +0900

    動く状態

--- ranking.cpp
+++ ranking.cpp
@@ -20,15 +20,17 @@ LPDIRECT3DVERTEXBUFFER9	g_pVtxBuffResult = NULL;					// 頂点バッファへのポインタ
 RESULT					g_aResult[MAX_RESULT];						// 今回の結果表示
 RESULT					g_aRanking[MAX_RESULT];						// ランキング表示
 RESULTMODE				g_resultMode;								// リザルト内のモード
+RESULTMODE				g_resultNext;								// 更新先のモード
+RESULTFADE				g_resultFade;
 P_RESULT				g_pThisRanking;								// 今回のランキングを指すポインタ
-int						g_nCounterEnding = 0;				// エンディングカウンター
+int						g_nCounterEnding = 0;						// エンディングカウンター
 int						g_nSelectResult;
 int						g_nThisScore;								// 今回のスコア
 int						g_nThisRank;								// 今回の順位
 int						g_nScoreMoveCouter;							// 今回のスコアが動くフレームカウンター
 int						g_aHighScore[MAX_NUMSCORE] =				// ランキング
 {
-	500,300,200,150,100,
+	50000,30000,20000,15000,10000,
 };
 const char*				g_aResultTexFile[] =
 {
@@ -43,13 +45,14 @@ const int				g_nMaxTexResult = sizeof g_aResultTexFile / sizeof(const char*);
 //**********************************************************************************
 //*** 定数変数 ***
 //**********************************************************************************
-constexpr int c_EndingTime = 240;		// エンディング持続時間
+constexpr int c_EndingTime = 300;		// エンディング持続時間
 
 //**************************************************************
 // プロトタイプ宣言
 int SortRanking(int nThisScore);
 void UpdateScore(void);
 void UpdateRank(void);
+void UpdateResultFade(void);
 void UpdateVtxResult(P_RESULT pResult);
 
 //=========================================================================================
@@ -64,17 +67,19 @@ void InitRanking(void)
 	vec3				pos, size;
 	int					nCntResult = 0;
 	int					nCntRanking = 0;
-	int					nThisScore = GetScore();
+	int					nThisScore = 40000;// GetScore();
 	int					nThisRank;
 
 	//**************************************************************
 	// 変数初期化
 	memset(&g_aResult[0], 0, sizeof(RESULT) * MAX_RESULT);
 	memset(&g_aRanking[0], 0, sizeof(RESULT) * MAX_RESULT);
-	g_resultMode = RESULT_SCORE;
+	g_resultMode = RESULT_NONE;
+	g_resultNext = RESULT_NONE;
+	g_resultFade = RESULTFADE_NONE;
 	g_nScoreMoveCouter = SCOREMOVE;
 	g_nSelectResult = 0;
-	g_nThisScore = GetScore();
+	g_nThisScore = 40000;// GetScore();
 	nThisRank = SortRanking(g_nThisScore);
 	g_pThisRanking = NULL;
 
@@ -96,42 +101,38 @@ void InitRanking(void)
 
 	//**************************************************************
 	//  リザルト情報初期化
-	pResult = GetThisScore();
-
 	// "RESULT"
 	pos = vec3(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.15f, 0.0f);
 	size = vec3(SCREEN_WIDTH * 0.25f, SCREEN_HEIGHT * 0.1f, 0.0f);
-	SetResult(pos, size, vec3_ZERO, colX_RED, RESULTTEX_RESULT, -1, pResult);
+	SetResult(pos, size, vec3_ZERO, colX_RED, RESULTTEX_RESULT, -1, GetThisScore());
 
 	// "SCORE"
 	pos = vec3(SCREEN_WIDTH * 0.4f, SCREEN_HEIGHT * 0.4f, 0.0f);
 	size = vec3(SCREEN_WIDTH * 0.2f, SCREEN_HEIGHT * 0.06f, 0.0f);
-	SetResult(pos, size, vec3_ZERO, colX_BLUE, RESULTTEX_SCORE, -1, pResult);
+	SetResult(pos, size, vec3_ZERO, colX_BLUE, RESULTTEX_SCORE, -1, GetThisScore());
 
 	// 今回のスコア
 	for (int nCntNum = MAX_SCOREDIGIT - 1; 0 <= nCntNum; nCntNum--)
 	{
 		pos = vec3(SCREEN_WIDTH * 0.65f + ((nCntNum - 1) * 40.0f),SCREEN_HEIGHT * 0.4f, 0.0f);
 		size = vec3(SCREEN_WIDTH * 0.018f, SCREEN_HEIGHT * 0.06f, 0.0f);
-		SetResult(pos, size, vec3_ZERO, colX_WHITE, RESULTTEX_NUM, nThisScore % 10, pResult);
+		SetResult(pos, size, vec3_ZERO, colX_WHITE, RESULTTEX_NUM, nThisScore % 10, GetThisScore());
 		nThisScore *= 0.1f;
 	}
 
 	//**************************************************************
 	//  ランキング初期化
-	pResult = GetRanking();
-
 	// "RANKING"
 	pos = vec3(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.15f, 0.0f);
 	size = vec3(SCREEN_WIDTH * 0.25f, SCREEN_HEIGHT * 0.1f, 0.0f);
-	SetResult(pos, size, vec3_ZERO, colX_WHITE, RESULTTEX_RANK, -1, pResult);
+	SetResult(pos, size, vec3_ZERO, colX_WHITE, RESULTTEX_RANK, -1, GetRanking());
 
 	// 順位
 	for (int nCntRank = 0; nCntRank < MAX_NUMSCORE; nCntRank++)
 	{
 		pos = vec3(SCREEN_WIDTH * 0.35f, SCREEN_HEIGHT * (nCntRank + 2) * 0.15f, 0.0f);
 		size = vec3(SCREEN_WIDTH * 0.043f, SCREEN_HEIGHT * 0.08f, 0.0f);
-		SetResult(pos, size, vec3_ZERO, colX_WHITE, RESULTTEX_RANKNUM, nCntRank, pResult);
+		SetResult(pos, size, vec3_ZERO, colX_WHITE, RESULTTEX_RANKNUM, nCntRank, GetRanking());
 	}
 
 	// スコア
@@ -144,20 +145,21 @@ void InitRanking(void)
 			size = vec3(SCREEN_WIDTH * 0.03f, SCREEN_HEIGHT * 0.06f, 0.0f);
 			if (nCntNum == MAX_SCOREDIGIT - 1 && nThisRank == nCntRank)
 			{// 今回のスコアの順位の一つ目のアドレス
-				g_pThisRanking = SetResult(pos, size, vec3_ZERO, colX_WHITE, RESULTTEX_NUM, nThisScore % 10, pResult);
+				g_pThisRanking = SetResult(pos, size, vec3_ZERO, colX_WHITE, RESULTTEX_NUM, nThisScore % 10, GetRanking());
 			}
 			else
 			{
-				SetResult(pos, size, vec3_ZERO, colX_WHITE, RESULTTEX_NUM, nThisScore % 10, pResult);
+				SetResult(pos, size, vec3_ZERO, colX_WHITE, RESULTTEX_NUM, nThisScore % 10, GetRanking());
 			}
 			nThisScore *= 0.1f;
 		}
 	}	
-
 	g_nCounterEnding = 0;
 
 	// リザルト内のモード
-	SetResultMode(g_resultMode);
+	SetResultMode(RESULT_SCORE);
+
+	EndDevice();
 }
 
 //=========================================================================================
@@ -217,23 +219,31 @@ void UninitRanking(void)
 //=========================================================================================
 void UpdateRanking(void)
 {
-	switch (g_resultMode)
+	if (g_resultFade == RESULTFADE_NONE)
 	{
-	//**************************************************************
-	// クリア画面更新
-	case RESULT_SCORE:
-		UpdateScore();
-		break;
+		switch (g_resultMode)
+		{
+			//**************************************************************
+			// クリア画面更新
+		case RESULT_SCORE:
+			UpdateScore();
+			break;
 
-	//**************************************************************
-	// ランキング更新
-	case RESULT_RANGING:
-		UpdateRank();
-		break;
+			//**************************************************************
+			// ランキング更新
+		case RESULT_RANGING:
+			UpdateRank();
+			break;
 
-	default:
-		break;
+		default:
+			break;
+		}
 	}
+	else
+	{
+		UpdateResultFade();
+	}
+
 	g_nCounterEnding++;
 }
 
@@ -253,6 +263,21 @@ void UpdateScore(void)
 			SetResultMode(RESULT_RANGING);
 			g_nCounterEnding = 0;
 		}
+	}
+
+	P_RESULT pResult = GetThisScore();
+	pResult += 2;
+
+	//**************************************************************
+	// スコアのアニメーション
+	for (int nCntNum = 0; nCntNum < MAX_SCOREDIGIT; nCntNum++)
+	{
+		(pResult + nCntNum)->rot.x += 0.05f;
+		(pResult + nCntNum)->pos.y += sinf((pResult + nCntNum)->rot.x + nCntNum) * 0.3f;
+
+		(pResult + nCntNum)->col.r = 1 - sinf((pResult + nCntNum)->rot.x) * 0.3f;
+		(pResult + nCntNum)->col.g = 1 - sinf((pResult + nCntNum)->rot.x) * 0.3f;
+		(pResult + nCntNum)->col.b = 1 - sinf((pResult + nCntNum)->rot.x) * 0.3f;
 	}
 
 	//**************************************************************
@@ -296,17 +321,13 @@ void UpdateRank(void)
 
 			// 色の変化
 			(g_pThisRanking + nCntNum)->rot.x += 0.05f;
+			(g_pThisRanking + nCntNum)->pos.y += sinf((g_pThisRanking + nCntNum)->rot.x + nCntNum) * 0.3f;
+
 			(g_pThisRanking + nCntNum)->col.r = 1 - sinf((g_pThisRanking + nCntNum)->rot.x) * 0.3f;
 			(g_pThisRanking + nCntNum)->col.g = 1 - sinf((g_pThisRanking + nCntNum)->rot.x) * 0.3f;
 			(g_pThisRanking + nCntNum)->col.b = 1 - sinf((g_pThisRanking + nCntNum)->rot.x) * 0.3f;
 		}
 		g_nScoreMoveCouter--;
-	}
-	//**************************************************************
-	// 前へ
-	if (GetKeyboardTrigger(KEY_BACK))
-	{
-		SetResultMode(RESULT_SCORE);
 	}
 
 	//**************************************************************
@@ -315,17 +336,44 @@ void UpdateRank(void)
 }
 
 //=========================================================================================
+// フェードの更新
+void UpdateResultFade(void)
+{
+	P_RESULT pResult = GetThisModeRanking();
+	
+	if (g_resultFade == RESULTFADE_IN)
+	{
+		g_resultFade = RESULTFADE_NONE;
+	}
+	else if (g_resultFade == RESULTFADE_OUT)
+	{
+		if (g_resultMode == RESULT_NONE || RESULT_MAX < g_resultMode)
+		{
+			g_resultMode = g_resultNext;
+			g_resultFade = RESULTFADE_IN;
+		}
+		else if(pResult)
+		{
+			g_resultMode = g_resultNext;
+			g_resultFade = RESULTFADE_IN;
+		}
+	}
+
+	UpdateVtxResult(GetThisModeRanking());
+}
+
+//=========================================================================================
 // 頂点更新
 void UpdateVtxResult(P_RESULT pResult)
 {
 	//**************************************************************
 	// 変数宣言
-	VERTEX_2D* pVtx;				// 頂点情報へのポインタ
-	int					nTexPosMax;
+	VERTEX_2D*	pVtx;				// 頂点情報へのポインタ
+	int			nTexPosMax;
 
 	//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 	// 頂点バッファをロックし、頂点情報へのポインタを取得
-	if (g_pVtxBuffResult)
+	if (g_pVtxBuffResult && pResult)
 	{
 		g_pVtxBuffResult->Lock(0, 0, (void**)&pVtx, 0);
 		for (int nCntResult = 0; nCntResult < MAX_RESULT; nCntResult++, pResult++)
@@ -438,6 +486,7 @@ void DrawRanking(void)
 			}
 		}
 	}
+	EndDevice();
 }
 
 //=========================================================================================
@@ -457,27 +506,33 @@ P_RESULT GetRanking(void)
 }
 
 //=========================================================================================
-// リザルトモードセット
+// リザルト情報取得
 //=========================================================================================
-void SetResultMode(RESULTMODE mode)
+P_RESULT GetThisModeRanking(void)
 {
-	switch (mode)
+	switch (g_resultMode)
 	{
-	//**************************************************************
-	// スコア表示
 	case RESULT_SCORE:
+		return GetThisScore();
 		break;
 
-	//**************************************************************
-	// ランキング表示
-	case RESULT_RANGING:
+	case  RESULT_RANGING:
+		return GetRanking();
 		break;
 
 	default:
+		return NULL;
 		break;
 	}
+}
 
-	g_resultMode = mode;
+//=========================================================================================
+// リザルトモードセット
+//=========================================================================================
+void SetResultMode(RESULTMODE next)
+{
+	g_resultNext = next;
+	g_resultFade = RESULTFADE_OUT;
 }
 
 //=========================================================================================
